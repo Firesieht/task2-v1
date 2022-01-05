d4327ea0461ded278273312a4460a9734375cb02
Firesieht <firesieht@mail.ru>
2022-01-06 00:32:20 +0300

    init commit

--- src/index.ts
+++ src/index.ts
@@ -2,69 +2,119 @@
 /* eslint-disable no-param-reassign */
 import * as BABYLON from 'babylonjs';
 import * as ZapparBabylon from '@zappar/zappar-babylonjs';
-import faceMeshTexture from '../assets/faceMeshTemplate.png';
+import * as MAT from 'babylonjs-materials';
+import target from '../assets/marker.zpt';
 import 'babylonjs-loaders';
 import './index.sass';
+import * as GUI from "@babylonjs/gui"
 
-// The SDK is supported on many different browsers, but there are some that
-// don't provide camera access. This function detects if the browser is supported
-// For more information on support, check out the readme over at
-// https://www.npmjs.com/package/@zappar/zappar-babylonjs
+
 if (ZapparBabylon.browserIncompatible()) {
-  // The browserIncompatibleUI() function shows a full-page dialog that informs the user
-  // they're using an unsupported browser, and provides a button to 'copy' the current page
-  // URL so they can 'paste' it into the address bar of a compatible alternative.
   ZapparBabylon.browserIncompatibleUI();
-
-  // If the browser is not compatible, we can avoid setting up the rest of the page
-  // so we throw an exception here.
   throw new Error('Unsupported browser');
 }
 
-// Setup BabylonJS in the usual way
 const canvas = document.createElement('canvas');
 document.body.appendChild(canvas);
 
 const engine = new BABYLON.Engine(canvas, true);
 
-export const scene = new BABYLON.Scene(engine);
-// eslint-disable-next-line no-unused-vars
-const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
+const scene = new BABYLON.Scene(engine);
 
-// Setup a Zappar camera instead of one of Babylon's cameras
-export const camera = new ZapparBabylon.Camera('ZapparCamera', scene);
+const light = new BABYLON.DirectionalLight('dir02', new BABYLON.Vector3(0, 0, -1), scene);
+light.position = new BABYLON.Vector3(0, 1, -10);
 
-// Request the necessary permission from the user
+const light1 = new BABYLON.HemisphericLight('hemi', new BABYLON.Vector3(1, -1, 0), scene);
+const shadowGenerator = new BABYLON.ShadowGenerator(1024, light);
+shadowGenerator.usePoissonSampling = true;
+
+const camera = new ZapparBabylon.Camera('ZapparCamera', scene);
+
 ZapparBabylon.permissionRequestUI().then((granted) => {
-  if (granted) camera.start(true);
+  if (granted) camera.start();
   else ZapparBabylon.permissionDeniedUI();
 });
 
-const faceTracker = new ZapparBabylon.FaceTrackerLoader().load();
-const trackerTransformNode = new ZapparBabylon.FaceTrackerTransformNode('tracker', camera, faceTracker, scene);
+const imageTracker = new ZapparBabylon.ImageTrackerLoader().load(target);
+const trackerTransformNode = new ZapparBabylon.ImageAnchorTransformNode('tracker', camera, imageTracker, scene);
 
 trackerTransformNode.setEnabled(false);
-faceTracker.onVisible.bind(() => {
+imageTracker.onVisible.bind(() => {
   trackerTransformNode.setEnabled(true);
 });
-faceTracker.onNotVisible.bind(() => {
+imageTracker.onNotVisible.bind(() => {
   trackerTransformNode.setEnabled(false);
 });
 
-const material = new BABYLON.StandardMaterial('mat', scene);
-material.diffuseTexture = new BABYLON.Texture(faceMeshTexture, scene);
+const ground = BABYLON.MeshBuilder.CreatePlane('plane', { width: 3.1, height: 2 }, scene);
 
-const faceMesh = new ZapparBabylon.FaceMeshGeometry('mesh', scene);
-faceMesh.parent = trackerTransformNode;
-faceMesh.material = material;
+const shadowMaterial = new MAT.ShadowOnlyMaterial('mat', scene);
+shadowMaterial.alpha = 0.5;
+shadowMaterial.activeLight = light;
+ground.material = shadowMaterial;
+
+
+
+ground.parent = trackerTransformNode;
+light.parent = trackerTransformNode;
+
+
+ground.receiveShadows = true;
+shadowGenerator.getShadowMap()?.renderList?.push(ground);
+
 
 window.addEventListener('resize', () => {
   engine.resize();
 });
 
-// Set up our render loop
+let phrases = [
+    "Хорошего нового года!",
+    "Исполнения всех желаний и мечт",
+    "Побольше любви и доверия",
+    "Много подарков и хорошего настроения"
+]
+var plane = BABYLON.Mesh.CreatePlane("plane", 2, scene);
+plane.parent = ground;
+plane.position.y = -0.5;
+var advancedTexture = GUI.AdvancedDynamicTexture.CreateForMesh(plane as any);
+
+var plane_phrase = BABYLON.Mesh.CreatePlane("plane_phrase", 3, scene); 
+plane_phrase.parent = ground;
+plane_phrase.position.y = 0.5;
+
+var advancedTexture2 = GUI.AdvancedDynamicTexture.CreateForMesh(plane_phrase as any);
+var phrase = GUI.Button.CreateSimpleButton("but", phrases[Math.floor(Math.random()*phrases.length)]);
+phrase.width = 0.7;
+phrase.height = 0.25;
+phrase.color = "white";
+phrase.fontSize = 50;
+phrase.background = "#817AA0";
+phrase.cornerRadius = 30
+phrase.pointerEnterAnimation = phrase.pointerOutAnimation = phrase.pointerDownAnimation = phrase.pointerUpAnimation = () => null
+phrase.thickness = 0
+
+
+var button1 = GUI.Button.CreateSimpleButton("but1", "Сменить");
+button1.width = 0.5;
+button1.height = 0.2;
+button1.color = "white";
+button1.fontSize = 50;
+button1.background = "#CE3923";
+button1.cornerRadius = 35;
+button1.hoverCursor = "pointer"
+button1.thickness = 0
+
+button1.onPointerUpObservable.add(function() {
+    let phrase_any = phrase as any
+    phrase_any.children[0].text = phrases[Math.floor(Math.random()*phrases.length)];
+    
+});
+
+
+advancedTexture.addControl(button1);
+advancedTexture2.addControl(phrase)
+
 engine.runRenderLoop(() => {
-  faceMesh.updateFromFaceTracker(faceTracker);
   camera.updateFrame();
   scene.render();
 });
